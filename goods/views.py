9b5bd7cf8aeb759744c2edf8766d950208f94b3d
lucius-hyeon <lucius.hyeon@gmail.com>
2022-12-02 14:27:39 +0900

    [:sparkles: Feat] 경매 상품 등록 기능 추가
    - Goods 모델 특성 변경
    - 경매 상품 등록 기능 추가

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Goods, BidPrice, GoodsImage
+from user.models import User
+from .serializers import GoodsSerializer, GoodsImageSerializer,BidPriceSerializer
+from chat.serializers import AuctionChatRoom
+
+class GoodsAPI(APIView):
+    def get(self, request):
+        goods = Goods.objects.all()
+        serializer = GoodsSerializer(goods, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        auction_room = AuctionChatRoom.objects.create()
+        serializer = GoodsSerializer(data=request.data, context={'auction_room':auction_room})
+        
+        if serializer.is_valid():
+            serializer.save(seller=request.user)
+            
+            return Response(serializer.data)
+        else:
+            return Response({'message': serializer.errors})
