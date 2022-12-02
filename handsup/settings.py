0073910d09fd49a945fdddec86eee3763795973d
lucius-hyeon <lucius.hyeon@gmail.com>
2022-12-02 20:06:23 +0900

    :sparkles: [ Feat ] crontab 설치

--- handsup/settings.py
+++ handsup/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django_crontab',
+    
     # service app
     'user',
     'review',
