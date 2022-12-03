bac287ef6a1a5aeeb506598ca35dae75b10519bf
Kim GyeongMin <gyeongmin951@naver.com>
2022-12-03 13:15:07 +0900

    :bulb: [ Update ] 메시지와 입찰가격에 따른 로직 분리
    - 메시지를 받았을 때, 유저id, 상품id, is_money의 값이 존재하는지 필터링 합니다.
    - 공통적으로 User의 인스턴스를 가져옵니다.
    - 돈일 경우 Goods의 인스턴스를 가져옵니다.
      - 상품이 없거나, 판매자가 입찰을 한 경우를 False로 반환합니다.( 추후에 exception을 반환)
      - 프론트에서 유효성 검사를 하는 가격을 백엔드에서 한 번 더 입찰가가 담겨있는 'message'를 int로 변환하여 검사합니다.
      - set_high_price()를 통하여 입찰가가 현재 최고가 보다 높은지 판단후 user_id와 high_price를 갱신해 줍니다.
    - 채팅 메시지와 입찰가 메시지에 따른 정보를 넣어서 보내줍니다.
      - 채팅 메시지 : 날짜, 오전 오후 + 시간, username, user_id, goods_id
      - 입찰 메시지 : username, user_id, goods_id, high_price

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,20 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+# utils
 import json
+from datetime import datetime
+
+# channels
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.auth import get_user_model
+
+# models
+from goods.models import Goods, BidPrice
+from user.models import User
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
-
-    cnt_join = 0
-
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_id']
+        self.room_name = self.scope['url_route']['kwargs']['goods_id']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
@@ -25,24 +33,107 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from WebSocket
+    # TODO user permission
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
 
-        # Send message to room group
+        text_data_json = json.loads(text_data)
+        is_money = text_data_json.get('is_money', '')
+        goods_id = text_data_json.get('goods_id', '')
+        user_id = text_data_json.get('goods_id', '')
+
+
+        # TODO return exception 찾아보기
+        if '' in [user_id, goods_id, is_money]:
+          return '?'
+
+        user = await self.get_user_obj(user_id)
+
+        if is_money == True:
+          goods = await self.get_goods_obj(goods_id)
+
+          if goods == False: # 방어 코드 (상품 x)
+            return False
+
+          if goods.seller_id == user_id: # 방어코드 (주최자가 경매입찰)
+            return False
+
+          try: # 방어 코드
+            money = int(text_data_json['message'])
+          except ValueError:
+            return False
+          
+          await self.set_high_price(goods, user_id, money)
+
+          response = {
+            'sender': user.id,
+            'sender_name': user.username,
+            'goods_id': goods_id,
+            'high_price' : money
+          }
+
+        else:
+          message = text_data_json['message']
+
+
+          now = datetime.now()
+          am_pm = now.strftime('%p')      
+          now_time = now.strftime('%I:%M')
+          now_date = now.strftime('%Y년 %m월 %d일 %A')
+
+          if am_pm == 'AM':
+            now_time = f"오전 {now_time}"
+          else:
+            now_time = f"오후 {now_time}"
+
+          response = {
+            'message': message,
+            'sender': user.id,
+            'sender_name': user.username,
+            'goods_id': goods_id,
+            'date': now_date,
+            'time': now_time,
+          }
+
+          # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'response': json.dumps(response)
+
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event['message']
+        await self.send(text_data=event['response'])
+    
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+    @database_sync_to_async
+    def set_high_price(self, goods:object, user_id, money):
+
+        if goods.high_price > money: # 방어코드
+          return False
+
+        goods.high_price = money
+        goods.buyer_id = user_id
+        goods.save()
+          
+
+    @database_sync_to_async
+    def get_user_obj(self, user_id):
+
+      try:
+        obj = User.objects.get(pk = user_id)
+      except User.DoesNotExist:
+        return False
+      return obj
+
+    @database_sync_to_async
+    def get_goods_obj(self, goods_id):
+
+      try:
+        obj = Goods.objects.get(pk = goods_id)
+      except Goods.DoesNotExist:
+        return False
+      return obj
