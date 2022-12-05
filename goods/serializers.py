2b5b5851f45368de0fa775e73611a788ab6051ca
Yi wonchae <wonchae1019@gmail.com>
2022-12-05 13:24:44 +0900

    Feat : 상품등록 페이지
    상품을 등록하기 위한 페이지

--- goods/serializers.py
+++ goods/serializers.py
@@ -2,40 +2,36 @@ from rest_framework import serializers
 from .models import Goods,GoodsImage
 
 class GoodImageSerializer(serializers.ModelSerializer):
-
+    print('이미지 시리얼 라이즈')
     image = serializers.ImageField(use_url=True)
     
     print('goodsimage serializer')
     class Meta:
         model = GoodsImage
-        fields =('image',)
+        fields =['image',]
 
 
 class GoodsPostSerializer(serializers.ModelSerializer):
-    image = GoodImageSerializer(many=True, read_only = True)
+    image = GoodImageSerializer(many = True, read_only=True)
 
     def get_image(self, obj):
-        image = obj.goodsimage_set.all()
-        request = self.context['request']
-        print('serializer',request)
-        return GoodsPostSerializer(instance=image, many = True, context={'request':request})
-
+        image = obj.image.all()
+        # request = self.context['request']
+        # print('serializer',request)
+        return GoodsPostSerializer(instance=image, many = True, context = self.context).data
+        # context={'request':request}
     class Meta:
         model = Goods
-        fields = (
-        'seller', 'buyer','auction_room','title','content',
-        'category','status','predict_price','start_price','high_price',
-        'trade_room','start_date','start_time','created_at','like','image',
-        )
+        fields = '__all__'
         read_only_fields = ("seller",)
 
     def create(self, validated_data):
         goods = Goods.objects.create(**validated_data)
-        print(goods)
+        print(goods.id)
         images_data = self.context['request'].FILES
-        print(images_data)
+        # print('serializer 이미지 데이터',images_data)
         for image_date in images_data.getlist('image'):
-            print('image for ')
+            print('image for')
             GoodsImage.objects.create(goods = goods, image = image_date)
         return goods
 
--- goods/views.py
+++ goods/views.py
@@ -6,27 +6,47 @@ from .serializers import GoodsPostSerializer,GoodImageSerializer
 from .models import GoodsImage, Goods
 
 class GoodsPostView(APIView):
+
     def post(self, request):
         print('post views실행')
         user = request.user
-        print(request.data)
-        print(request.data['image'])
-        serialize_image = GoodImageSerializer(data = request.data['image'])
-        serialize_post = GoodsPostSerializer(data = request.data, context={'request':request}) #request받기
+        image={
+            "image" : request.data.get('image'),
+        }
+        data={
+        "title" : request.data.get('title'),
+        "category" : request.data.get('category'),
+        "start_date" : request.data.get('dateControl'),
+        "start_time" : request.data.get('timeControl'),
+        "content" : request.data.get('content'),
+        "auction_room" : request.data.get('auction_room'),
+        "predict_price" :request.data.get('predict_price'),
+        "trade_room" : request.data.get('trade_room'),
+        "start_price":request.data.get('start_price'),
+        }
+
+        print(data)
+        serialize_image = GoodImageSerializer(data = image)
+        serialize_post = GoodsPostSerializer(data = data, context={'request':request}) #request받기
         # 유효성 검사
         print('vaild 직전')
         if serialize_post.is_valid():
             serialize_post.save(seller = user)
-            print('저장완료?')
-            # print(serialize_post.data['image'])
-            # if serialize_post.data['image']:
-            #     pass
-            # GoodsImage.objects.create(
-            # goods = Goods.objects.get(id = serialize_post.data['id']),
-            # image = request.data['image']
-            # )
             return Response(serialize_post.data)
         print(serialize_post.errors)
         return Response(serialize_post.errors)
+
+    def get(self, request):
+        image = GoodsImage.objects.get(id=1)
+        post = Goods.objects.get(id = 8)
+
+        post_data = GoodsPostSerializer(post)
+        image_data = GoodImageSerializer(image)
+
+        data ={
+            "post":post_data.data,
+            "image":image_data.data
+        }
+        return Response(data)
 
 
