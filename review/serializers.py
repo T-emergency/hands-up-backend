ddab63fff4846e0cc7e0ea9ab7ec3ce174217587
minsu <splendent77@gmail.com>
2022-12-22 15:42:18 +0900

    :bulb: [ Update ] XSS 방지코드 작성
    시리얼라이저에서의 방지 escape함수 활용
    close #94

--- review/serializers.py
+++ review/serializers.py
@@ -1,21 +1,16 @@
 from rest_framework import serializers
 from .models import Review
 from user.models import User
+from django.utils.html import escape
+
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
-
     
-    # Object-level validation
-    # def validate(self, data):
-    #     print("밸리",data)
-    #     print("밸리",len(data['content']))
-    #     if len(data['content']) > 30:
-    #         raise serializers.ValidationError("Title and Description must be different")
-    #     return data
-    # def validate_content(self, value):
-    #     if len(value) >30:
-    #         raise serializers.ValidationError("Title should be less than 60 characters")
-
+    def create(self, validated_data):
+        review=super().create(validated_data)
+        review.content = escape(self.context['request'].data['content'])
+        review.save()
+        return review
 
     class Meta:
         model = Review
@@ -23,12 +18,12 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
         read_only_fields=('goods','author','receiver')
 
 class ReviewListSerializer(serializers.ModelSerializer):
-    # TODO 판매상품 4개
     author = serializers.SerializerMethodField()
     receiver = serializers.SerializerMethodField()
     created_at= serializers.SerializerMethodField()
 
     def get_author(self, obj):
+        print(obj.author.username)
         return obj.author.username
 
     def get_receiver(self, obj):
--- review/views.py
+++ review/views.py
@@ -12,18 +12,12 @@ from .models import Review
 from django.db.models import Q
 import datetime
 from datetime import timedelta
+
 class ReviewAPIView(APIView):
     # permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
         """
-        str(datetime.date.today())[:10]
         판매자 정보에 들어갔을때 후기모음
-        필요한 것
-        받은사람은사람 이름, 사진 | 리뷰 작성자 이름 사진 시간 내용 | 리뷰의 개수
-        리뷰갖고와서 receiver하면 유저정보가 없을 수 있음 그럼 user에 만들어둔걸로 유저 불러오고
-        카운트 어떻게 할건지
-        지금은 사실 유저의 데이터 불러오는게 중요
-        일단 원래대로 이미지 공부하고 성공하면 유저따로
         """
         user_id=request.query_params.get('user_id','')
         reviews=Review.objects.filter(receiver_id=user_id)
@@ -32,23 +26,16 @@ class ReviewAPIView(APIView):
         soso_review_count=0
         good_review_count=0
         excellent_review_count=0
-        print(reviews)
         for review in reviews:
             if review.score==-20:
-                print(review.score)
-                print(bad_review_count)
                 bad_review_count +=1
-                print(bad_review_count)
             elif review.score==0:
                 soso_review_count +=1
             elif review.score==3:
                 good_review_count +=1
             elif review.score==5:
                 excellent_review_count +=1
-        print("아래",bad_review_count)
-        print(soso_review_count)
-        print(good_review_count)
-        print(excellent_review_count)
+
         data = {
             "bad_review_count":bad_review_count,
             "soso_review_count":soso_review_count,
@@ -61,17 +48,14 @@ class ReviewAPIView(APIView):
         else:
             return Response(status=status.HTTP_200_OK)
 
-    # TODO post하면 바로 점수반영
     def post(self, request):
         """
         리뷰를 남기면서 상대방의 매너를 평가하는 기능
-        판매글에서 대화창에 들어왔을때 로컬스토리지, 쿼리파라미터로 받음
         """
-
         goods_id=request.query_params.get('goods_id','')
         goods_obj=Goods.objects.get(id=goods_id)
         review_exist=Review.objects.filter(goods_id=goods_id, author_id=request.user.id).exists()
-        serializer = ReviewCreateSerializer(data=request.data)
+        serializer = ReviewCreateSerializer(data=request.data, context={'request':request})
         score=(request.data.get('score'))
         if review_exist==True:
             """
@@ -84,9 +68,10 @@ class ReviewAPIView(APIView):
                 author 셀러일때 review의 receiver 저장
                 """
                 buyer=get_object_or_404(User, id=goods_obj.buyer_id)
+                serializer.save(author = request.user, receiver=buyer, goods = goods_obj)
                 buyer.rating_score = buyer.rating_score + int(score)*0.4
                 buyer.save()
-                serializer.save(author = request.user, receiver=buyer, goods = goods_obj) # 포린키에 저장하는건 id str이 아니라 객체임 그래서 객체가져와서 저장해야한다.
+                print(serializer.data)
                 if score != '-20':
                     return Response(serializer.data, status=status.HTTP_200_OK)
                 else:
@@ -103,13 +88,15 @@ class ReviewAPIView(APIView):
                         return Response({"message":"연속적인 비매너는 아니네요"}, status=status.HTTP_200_OK)
 
             elif serializer.is_valid() and request.user.id==goods_obj.buyer_id:
+
                 """
                 author 바이어일때 receiver 저장
                 """
                 seller=get_object_or_404(User, id=goods_obj.seller_id)
+                serializer.save(author = request.user, receiver=seller, goods = goods_obj) # 포린키에 저장하는건 id str이 아니라 객체임 그래서 객체가져와서 저장해야한다.
                 seller.rating_score = seller.rating_score + int(score)*0.4
                 seller.save()
-                serializer.save(author = request.user, receiver=seller, goods = goods_obj) # 포린키에 저장하는건 id str이 아니라 객체임 그래서 객체가져와서 저장해야한다.
+                print(serializer.data)
                 if score != '-20':
                     return Response(serializer.data, status=status.HTTP_200_OK)
                 else:
